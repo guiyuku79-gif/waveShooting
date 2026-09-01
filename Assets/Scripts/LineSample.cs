74d3ad6584c278a6ca6c1d1cce87ccbd07a486d1
origamimus <274768565+guiyuku79-gif@users.noreply.github.com>
2026-09-01 16:14:25 +0900

    波を遅くする、敵の波を自走で作る

--- Assets/Scripts/LineSample.cs
+++ Assets/Scripts/LineSample.cs
@@ -11,7 +11,9 @@ public class LineSample : MonoBehaviour
     public float frequency = 2f;
     public float width = 10f;
 
-    [SerializeField] private float moveInterval = 0.1f;
+    [SerializeField] private float originalMoveInterval = 0.1f;
+
+    private float moveInterval;
     private float deltaTimeCount;
     [SerializeField] public int division = 100;//100個の点で表現
 
@@ -20,6 +22,12 @@ public class LineSample : MonoBehaviour
 
     public List<float> EnemyWavePowers = new List<float>();
     public List<int> EnemyWaveIds = new List<int>();
+
+    //次に出す波のIDを決める
+    private int playerWaveId;
+    private int enemyWaveId;
+
+    private List<float> NextEnemyWaveList = new List<float>();
 
     public static LineSample Instance { get; private set; }
 
@@ -45,6 +53,15 @@ public class LineSample : MonoBehaviour
         }
 
         deltaTimeCount = 0f;
+
+        playerWaveId = 1;
+        enemyWaveId = 1;
+
+        //仮
+        for (int i = 0; i < 20; i++)
+        {
+            NextEnemyWaveList.Add(i * 0.1f);
+        }
     }
 
     void Update()
@@ -54,6 +71,13 @@ public class LineSample : MonoBehaviour
         {
             deltaTimeCount = 0;
             MoveWave();
+        }
+
+        moveInterval = IntervalChange();
+
+        if (Keyboard.current.aKey.wasPressedThisFrame)
+        {
+            MakeSinWave();
         }
     }
 
@@ -83,20 +107,11 @@ public class LineSample : MonoBehaviour
                 new Vector3(mouseScreenPos.x, mouseScreenPos.y, 10f)
             );
 
-            if (Keyboard.current.qKey.isPressed)
-            {
-                PlayerWaveIds[0] = 1;
-                PlayerWavePowers[0] = mouseWorldPos.y;
-                EnemyWavePowers[division - 1] = 0f;
-                EnemyWaveIds[division - 1] = 0;
-            }
-            else
-            {
-                PlayerWavePowers[0] = 0f;
-                PlayerWaveIds[0] = 0;
-                EnemyWavePowers[division - 1] = mouseWorldPos.y;
-                EnemyWaveIds[division - 1] = 1;
-            }
+
+            PlayerWaveIds[0] = playerWaveId;
+            PlayerWavePowers[0] = mouseWorldPos.y;
+            EnemyWavePowers[division - 1] = 0f;
+            EnemyWaveIds[division - 1] = 0;
 
         }
         else
@@ -104,8 +119,69 @@ public class LineSample : MonoBehaviour
             PlayerWavePowers[0] = 0f;
             PlayerWaveIds[0] = 0;
 
+        }
+
+        if (NextEnemyWaveList.Count == 0)
+        {
             EnemyWavePowers[division - 1] = 0f;
             EnemyWaveIds[division - 1] = 0;
+        }
+        else
+        {
+            EnemyWavePowers[division - 1] = NextEnemyWaveList[0];
+            EnemyWaveIds[division - 1] = enemyWaveId;
+            NextEnemyWaveList.RemoveAt(0);
+        }
+
+        WaveIDCheck();
+    }
+
+    private void WaveIDCheck()
+    {
+        if (PlayerWaveIds[0] == 0 && PlayerWaveIds[1] != 0)
+        {
+            playerWaveId++;
+            Debug.Log(playerWaveId);
+        }
+        if (EnemyWaveIds[division - 1] == 0 && EnemyWaveIds[division - 2] != 0)
+        {
+            enemyWaveId++;
+        }
+    }
+
+    //波が重なっているほど時間をゆっくりにする
+    private float IntervalChange()
+    {
+        int onCount = 0;
+        int offCount = 0;
+
+        for (int i = 0; i < division; i++)
+        {
+            if (PlayerWaveIds[i] != 0 && EnemyWaveIds[i] != 0)
+            {
+                onCount++;
+            }
+            else if (PlayerWaveIds[i] != 0 || EnemyWaveIds[i] != 0)
+            {
+                offCount++;
+            }
+        }
+
+
+
+        if (onCount + offCount == 0) return originalMoveInterval;
+
+
+        //int型同士の計算はintになってしまうので気を付ける
+        return originalMoveInterval * (1 + (float)onCount / (onCount + offCount));
+
+    }
+
+    private void MakeSinWave()
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            NextEnemyWaveList.Add(Mathf.Sin(Mathf.PI * i / 15));
         }
     }
 }
