ada66fe57218e7f12de949a69771ffaa7a5455b2
origamimus <274768565+guiyuku79-gif@users.noreply.github.com>
2026-09-17 20:16:29 +0900

    Mediumクラスの派生の準備

---
+++ Assets/Scripts/EnemyMedium.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using UnityEngine;
+using System.Data.Common; //Debug.Log用
+
+public class EnemyMedium : Medium
+{
+    public EnemyMedium(int division)
+    : base(division)
+    {
+    }
+
+
+}
--- Assets/Scripts/Medium.cs
+++ Assets/Scripts/Medium.cs
@@ -78,6 +78,7 @@ public class Medium
 
     }
 
+
     public void DestroyTooSmallWave()
     {
         foreach (int id in ToUniqueIds())
