170c3d0a676cf040d9429e67299ba743d7683450
origamimus <274768565+guiyuku79-gif@users.noreply.github.com>
2026-09-26 09:54:12 +0900

    敵のHPの減り方 Fixes #22 波のダメージを現実に合わせる Fixes #23

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,24 +1,43 @@
 using UnityEngine;
-
+using UnityEngine.UI;
 public class EnemyController : MonoBehaviour
 {
+    [SerializeField] Image HPBar;
 
     LineSample lineSample;
+
+    float maxHp = 5;
     float hp;
+
+    float priviousWavePower;
+    float priviousWaveDuration;
 
     public void Init(LineSample lineSample, int lane)
     {
         this.lineSample = lineSample;
-        hp = 10;
+        hp = maxHp;
         transform.position = new Vector3(Constants.Width / 2, Constants.laneYs[lane], 0);
 
         this.lineSample.WaveMoved += OnWaveMoved;
+
+        this.priviousWavePower = 0;
+        this.priviousWaveDuration = 0;
     }
     private void OnWaveMoved()
     {
-        if (lineSample.playerMedium.waveIds[^1] != 0)
+        float currentWavePower = lineSample.playerMedium.waveIds[^1];
+        if (currentWavePower != 0)
         {
-            hp -= Mathf.Abs(lineSample.playerMedium.wavePowers[^1]);
+            if (priviousWavePower * currentWavePower <= 0)
+            {
+                priviousWaveDuration = 0;
+            }
+            priviousWavePower = currentWavePower;
+            priviousWaveDuration += Constants.Width / Constants.Division;
+
+            hp -= Mathf.Abs(currentWavePower) / (0.5f + priviousWaveDuration) * Constants.Width / Constants.Division;
+
+            HPBar.fillAmount = hp / maxHp;
             if (hp <= 0)
             {
                 Destroy(gameObject);
